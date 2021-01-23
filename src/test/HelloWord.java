8d6f189185f17bc6c57cf053d754a4946c917b5e
LENOVO 330 <LENOVO 330@DESKTOP-16BSVEN>
2021-01-23 11:16:57 -0600

    version 1.1

---
+++ src/test/HelloWord.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author LENOVO 330
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("buenos dias mundo");
+        //metacomandos
+        //sout
+        //psvm
+        
+        
+        
+    }
+   
+            
+    
+}
