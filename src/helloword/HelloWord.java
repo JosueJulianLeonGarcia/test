fc75f1406757572616eb6e84541b0b7bf72bd900
LENOVO 330 <LENOVO 330@DESKTOP-16BSVEN>
2021-03-01 18:53:23 -0600

    1.1

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Nombre: Josue Julian Leon Garcia
+Matricula: 2022995
  */
 package helloword;
 
